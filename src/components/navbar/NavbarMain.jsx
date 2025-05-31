c38c19ad84288afda2be6b8455519db2ae8284e3
Rohit Kumar Morya <moryarohit7976@gmail.com>
2025-05-31 17:18:43 +0530

    sm issue fixed

--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -10,8 +10,8 @@ const NavbarMain = () => {
         setMenuOpen(!menuOpen);
     }
   return (
-   <nav className='w-full fixed left-1/2 transform -translate-x-1/2 z-20 flex gap-4 mt-2 px-4'>
-  <div className='flex justify-between w-full max-w-screen-xl mx-auto bg-black items-center p-6 rounded-full border-[0.5px] border-orange'>
+   <nav className='max-w-[1300px] mx-auto px-4 w-full fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2 '>
+  <div className='flex justify-between w-full max-w-[1200px] mx-auto bg-black items-center p-6 rounded-r-full rounded-l-full border-[0.5px] border-orange'>
 
          <NavbarLogo/>
             <div className={`${menuOpen? "sm:block " : "sm:hidden"}    lg:block` }>  
