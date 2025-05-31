ab7bc53d3894bc9c27b46ad32dfe15638fc16a30
Rohit Kumar Morya <moryarohit7976@gmail.com>
2025-05-31 19:55:57 +0530

    commit17

--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -9,28 +9,33 @@ const NavbarMain = () => {
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-   <nav className="w-full fixed  top-0 z-20 px-2 sm:px-4 mt-2">
-  <div className="w-full max-w-[95%] sm:max-w-[70%] lg:max-w-[1200px] mx-auto bg-black border border-orange rounded-full flex items-center justify-between px-4 py-3">
+    <nav className="w-full fixed z-20 left-0 top-0 px-2 sm:px-4 mt-2">
+      <div className="flex items-center justify-between bg-black border-[0.5px] border-orange rounded-full px-4 py-3 max-w-[95%] sm:max-w-[70%] lg:max-w-[1200px] mx-auto overflow-hidden">
+        
+     
+        <NavbarLogo />
+
     
-    {/* Left: Logo */}
-    <NavbarLogo />
+        <div className={`${menuOpen ? "sm:block" : "sm:hidden"} lg:block`}>
+          <NavbarLinks />
+        </div>
 
-    {/* Middle: Links */}
-    <div className={`${menuOpen ? "sm:block" : "sm:hidden"} lg:block`}>
-      <NavbarLinks />
-    </div>
-
-    {/* Right: Hire Me + Hamburger */}
-    <div className="flex items-center gap-2">
-      <NavbarBtn />
-      <div className="lg:hidden sm:block">
-        <button className="text-2xl p-2 border border-orange rounded-full text-white" onClick={toggleMenu}>
-          <GiHamburgerMenu />
-        </button>
+        {/* Hire Me Button + Hamburger grouped */}
+        <div className="flex items-center gap-2">
+          <NavbarBtn />
+          
+          {/* Hamburger - visible only on mobile */}
+          <div className="lg:hidden sm:block">
+            <button
+              className="text-2xl p-2 border border-orange rounded-full text-white"
+              onClick={toggleMenu}
+            >
+              <GiHamburgerMenu />
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</nav>
+    </nav>
   )
 }
 
