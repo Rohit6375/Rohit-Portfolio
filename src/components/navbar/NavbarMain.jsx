a7a7154fb7dc91041de84627fd858ab9b397df08
Rohit Kumar Morya <moryarohit7976@gmail.com>
2025-05-31 19:37:55 +0530

    commit17

--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -9,7 +9,7 @@ const NavbarMain = () => {
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-   <nav className="w-full fixed top-0 z-20 px-2 sm:px-4 mt-2">
+   <nav className="w-full fixed  top-0 z-20 px-2 sm:px-4 mt-2">
   <div className="w-full max-w-[95%] sm:max-w-[70%] lg:max-w-[1200px] mx-auto bg-black border border-orange rounded-full flex items-center justify-between px-4 py-3">
     
     {/* Left: Logo */}
